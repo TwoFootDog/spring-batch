11f17f3bc595ee6cce614f4bc2b69f7263423659
two dog <ekfrl2815@gmail.com>
2021-10-14 01:13:31 +0900

    1014_1

---
+++ src/main/java/co/kr/kgc/point/batch/job/listener/SampleJobListener.java
@@ -0,0 +1,177 @@
+package co.kr.kgc.point.batch.job.listener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.explore.JobExplorer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+public class SampleJobListener implements JobExecutionListener {
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        String jobName = jobExecution.getJobInstance().getJobName();
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        String jobName = jobExecution.getJobInstance().getJobName();
+        String exitCode = jobExecution.getExitStatus().getExitCode();
+    }
+}
+/*
+public class EventCouponIssueJobListener implements JobExecutionListener {
+
+    @Resource(name="commonDAO")
+    private CommonDAO commonDAO;
+
+    @Autowired
+    private JobExplorer jobExplorer;
+
+    @Resource(name = "messageSource")
+    private MessageSource messageSource;
+
+    private boolean isJobRunning;
+
+    private boolean existYn = false;
+
+    @Override
+    public void beforeJob(JobExecution jobExecution) {
+        String jobName = jobExecution.getJobInstance().getJobName();
+
+        JobLogger.info(jobName, "JobStartTime: "+jobExecution.getStartTime());
+        JobLogger.info(jobName, "JobParameters: "+jobExecution.getJobParameters().getParameters());
+        isJobRunning = false;
+        if (jobExplorer.findRunningJobExecutions(jobExecution.getJobInstance().getJobName()).size() > 1) {
+            isJobRunning = true;
+            throw new RuntimeException("This job is already running.: "+ jobExecution.getJobInstance().getJobName());
+        }
+
+        JobLogger.info(jobName, "checking config EVENT_COUPON");
+
+        String promoNo = commonDAO.getConfigVal("EVENT_COUPON_NO");
+
+        if( !"".equals(ComUtil.NVL(promoNo)) ) {
+            existYn = true;
+        } else {
+            throw new RuntimeException("config not exists !");
+        }
+
+    }
+
+    @Override
+    public void afterJob(JobExecution jobExecution) {
+        String jobName = jobExecution.getJobInstance().getJobName();
+        String exitCode = jobExecution.getExitStatus().getExitCode();
+        String exitMsg = null;
+        if ("COMPLETED".equals(exitCode)) {
+            exitMsg = messageSource.getMessage("batch.job.processed", new String[]{}, null);
+        }else if ("FAILED".equals(exitCode)) {
+            if (isJobRunning) {
+                exitMsg = messageSource.getMessage("batch.job.executing", new String[]{}, null);
+            }else {
+                if( !existYn ) {
+                    exitCode = "STOPPED";
+                    exitMsg = "프로모션 기간이 아닙니다.";
+                }
+            }
+        }else if ("EXECUTING".equals(exitCode)) {
+            exitMsg = messageSource.getMessage("batch.job.executing", new String[]{}, null);
+        }else if ("NOOP".equals(exitCode)) {
+            exitMsg = messageSource.getMessage("batch.job.noop", new String[]{}, null);
+        }else if ("STOPPED".equals(exitCode)) {
+            exitMsg = messageSource.getMessage("batch.job.stopped", new String[]{}, null);
+        }else {
+            exitMsg = messageSource.getMessage("batch.job.processed", new String[]{}, null);
+        }
+        JobLogger.info(jobName, "JobEndTime: "+jobExecution.getEndTime());
+        JobLogger.info(jobName, exitCode + ": " + exitMsg);
+        if ("FAILED".equals(exitCode)) 	JobLogger.info(jobName, jobExecution.getExitStatus().getExitDescription());
+
+        jobExecution.setExitStatus(new ExitStatus(exitCode, exitMsg));
+    }
+
+}*/
+
+/*
+
+
+public class AutoRefundPaStepListener implements StepExecutionListener {
+
+    @Resource(name = "messageSource")
+    private MessageSource messageSource;
+
+    @Resource(name = "commonDAO")
+    private CommonDAO commonDAO;
+
+    private static final Logger log = LoggerFactory.getLogger(AutoRefundPaStepListener.class);
+
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+        String jobName = stepExecution.getJobExecution().getJobInstance().getJobName();
+
+        JobLogger.info(jobName, "Step: " + stepExecution.getStepName());
+        JobLogger.info(jobName, "StartTime: " + stepExecution.getStartTime());
+
+        try {
+            Map<String, JobParameter> parameter = stepExecution.getJobParameters().getParameters();
+            Map<String, Object> batchParameter = commonDAO.setDefaultParameter(parameter);
+
+            batchParameter.put("jobName", jobName);
+            batchParameter.put("reqUrl", PropertyUtil.getString("BACK_OFFICE_SERVER")+"/orderclaim/autorefund.do?");
+
+            stepExecution.getExecutionContext().put("batchParameter", batchParameter);
+            JobLogger.info(jobName, "batchParameter: " + batchParameter);
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.toString());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+
+        String jobName = stepExecution.getJobExecution().getJobInstance().getJobName();
+        String exitCode = stepExecution.getExitStatus().getExitCode();
+        String exitMsg = null;
+
+        if ("COMPLETED".equals(exitCode)) {
+            if (stepExecution.getReadCount() > 0) {
+                if (stepExecution.getWriteSkipCount() > 0) {
+                    exitMsg = messageSource.getMessage("batch.job.skipped",
+                            new String[] { String.valueOf(stepExecution.getReadCount()), String.valueOf(stepExecution.getWriteCount()) }, null);
+                } else {
+                    exitMsg = messageSource.getMessage("batch.job.completed", new String[] { String.valueOf(stepExecution.getReadCount()) }, null);
+                }
+            } else {
+                exitMsg = messageSource.getMessage("batch.job.nodata", new String[] {}, null);
+            }
+        } else if ("FAILED".equals(exitCode)) {
+            exitMsg = messageSource.getMessage("batch.job.failed", new String[] {}, null);
+        } else if ("EXECUTING".equals(exitCode)) {
+            exitMsg = messageSource.getMessage("batch.job.executing", new String[] {}, null);
+        } else if ("NOOP".equals(exitCode)) {
+            exitMsg = messageSource.getMessage("batch.job.noop", new String[] {}, null);
+        } else if ("STOPPED".equals(exitCode)) {
+            exitMsg = messageSource.getMessage("batch.job.stopped", new String[] {}, null);
+        } else {
+            exitMsg = messageSource.getMessage("batch.job.processed", new String[] {}, null);
+        }
+
+        JobLogger.info(jobName, "Total Count: " + stepExecution.getReadCount());
+        JobLogger.info(jobName, "Success Count: " + stepExecution.getWriteCount());
+        JobLogger.info(jobName, "Faild Count: " + stepExecution.getWriteSkipCount());
+        JobLogger.info(jobName, exitCode + ": " + exitMsg);
+
+        if ("FAILED".equals(exitCode))
+            JobLogger.info(jobName, stepExecution.getExitStatus().getExitDescription());
+
+        return new ExitStatus(exitCode, exitMsg);
+    }
+
+}
+*/
---
+++ src/main/java/co/kr/kgc/point/batch/job/listener/SampleStepListener.java
@@ -0,0 +1,17 @@
+package co.kr.kgc.point.batch.job.listener;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.StepExecutionListener;
+
+public class SampleStepListener implements StepExecutionListener {
+    @Override
+    public void beforeStep(StepExecution stepExecution) {
+
+    }
+
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        return null;
+    }
+}
